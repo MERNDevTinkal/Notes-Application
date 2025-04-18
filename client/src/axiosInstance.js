9462cf7dbbc3a52506bb82b2c5c34abb7b644837
Tinkal Kumar <tinkalkumar67693@gmail.com>
2025-04-18 14:48:22 +0530

    changed url

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: 'https://notes-application-2-vv1k.onrender.com/api/v1',
   withCredentials: true, //  Send cookies in requests
 })
 
